55bbc32e9263ee8df9145dd14a79fe33f341035c
MalshaL <malsha.lr946@gmail.com>
2016-10-08 14:43:51 +0530

    Added login interface

--- CodeRush/CodeRush/app/src/main/java/com/example/hp/coderush/MainActivity.java
+++ CodeRush/CodeRush/app/src/main/java/com/example/hp/coderush/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.hp.coderush;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Intent loginIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        loginIntent = new Intent(this, LoginActivity.class);
+        startActivity(loginIntent);
     }
 }
